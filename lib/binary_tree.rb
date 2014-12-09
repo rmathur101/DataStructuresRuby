4adab92240b84c5029bfa59c1e8c5fbb89d15242
Rohan Mathur <rmathur101@gmail.com>
2014-12-09 14:49:46 -0600

    all tests passing

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -1,6 +1,6 @@
 class TreeNode
-	attr_accessor :data, :right_node, :left_node
-	def initialize(data, right_node=nil, left_node = nil)
+	attr_accessor :data, :right, :left
+	def initialize(data, left_node = nil, right_node=nil)
 		@data = data
 		@right = right_node
 		@left = left_node
@@ -8,5 +8,27 @@ class TreeNode
 end
 
 class Tree
+	include Enumerable
+
 	attr_accessor :root
+
+	def initialize
+		@root = nil
+	end
+
+	def insert(value)
+		return self.root = insert_node(root, value)
+	end
+
+	private
+
+	def insert_node(node, value)
+		return TreeNode.new(value) if node == nil
+		if node.data > value
+			node.left = insert_node(node.left, value)
+		elsif node.data <= value
+			node.right = insert_node(node.right, value)
+		end
+		return node
+	end
 end
--- spec/binary_tree_spec.rb
+++ spec/binary_tree_spec.rb
@@ -1,9 +1,40 @@
 require "rspec"
-require_relative "../lib/linked_list.rb"
+require_relative "../lib/binary_tree.rb"
 
 describe "Tree" do
 	it "defaults to a root value of nil " do
 		tree = Tree.new
 		expect(tree.root).to eq(nil)
+	end
+
+	it "is defined such that each node has a value less than the left node and greater than or equal to the right node" do
+		tree = Tree.new
+		node = TreeNode.new(20, TreeNode.new(10), TreeNode.new(30))
+		tree.root = node
+
+		expect(tree.root.data).to eq(20)
+		expect(tree.root.left.data).to eq(10)
+		expect(tree.root.right.data).to eq(30)
+	end
+
+	describe "#insert" do
+		context "when value indicates a left node" do
+
+		end
+		it "inserts a node with data that equals such that it is an appropriate left node" do
+			tree = Tree.new
+			node = TreeNode.new(20, TreeNode.new(10), TreeNode.new(30))
+			tree.root = node
+			tree.insert(5)
+
+			expect(tree.root.data).to eq(20)
+			expect(tree.root.left.data).to eq(10)
+			expect(tree.root.right.data).to eq(30)
+
+			expect(tree.root.left.left.data).to eq(5)
+			expect(tree.root.left.right).to eq(nil)
+
+		end
+
 	end
 end
